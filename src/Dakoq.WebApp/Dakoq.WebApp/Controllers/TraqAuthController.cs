592db09f3d48f5a87cae6fb5de61767ea0553fcc
YuHima03 <yuhima03@gmail.com>
2025-02-16 16:00:41 +0900

    ✨ traQ の OAuth 認可のコールバック (仮実装)

---
+++ src/Dakoq.WebApp/Dakoq.WebApp/Controllers/TraqAuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dakoq.WebApp.Controllers
+{
+    [ApiController]
+    [Route("/auth/traq")]
+    public class TraqAuthController(IHttpClientFactory httpClientFactory) : ControllerBase
+    {
+        readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+        [Route("[action]")]
+        [HttpGet]
+        public async Task<IActionResult> CallbackAsync([FromQuery(Name = "code")] string? code)
+        {
+            return Forbid();
+
+            Traq.Api.Oauth2Api traqOAuthApi = new(_httpClientFactory.CreateClient("traQ"));
+            var token = await traqOAuthApi.PostOAuth2TokenWithHttpInfoAsync(
+                grantType: "authorization_code",
+                clientId: "",
+                code: code
+            );
+
+            return Ok();
+        }
+    }
+}
