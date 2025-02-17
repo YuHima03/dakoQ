bc3648787024411ece77feab10b0b7a6a0876cc8
YuHima03 <yuhima03@gmail.com>
2025-02-17 14:04:53 +0900

    ✨ AppConfiguration クラスの実装

--- src/Dakoq.WebApp/Dakoq.WebApp/AppConfiguration.cs
+++ src/Dakoq.WebApp/Dakoq.WebApp/AppConfiguration.cs
@@ -2,6 +2,19 @@
 {
     public class AppConfiguration
     {
+        public Uri? DakoqBaseAddress { get; set; }
 
+        public Uri? TraqApiBaseAddress { get; set; }
+
+        public KnoqAuthenticationInfo? KnoqAuthInfo { get; set; }
+
+        public string? DbConnectionString { get; set; }
+    }
+
+    public sealed class KnoqAuthenticationInfo
+    {
+        public string? TraqUsername { get; set; }
+
+        public string? TraqPassword { get; set; }
     }
 }
