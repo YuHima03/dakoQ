825e08d10daf42170b46a8fb60ccbc7c835c92a5
YuHima03 <yuhima03@gmail.com>
2025-02-19 00:22:41 +0900

    ✨ AuthUserInfo

---
+++ src/Dakoq.WebApp/Dakoq.WebApp/AuthUserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Dakoq.WebApp
+{
+    public class AuthUserInfo
+    {
+        public Guid Id { get; set; }
+
+        [NotNull]
+        public string? Name { get; set; }
+
+        [NotNull]
+        public string? AccessToken { get; set; }
+
+        public DateTimeOffset TokenExpiresAt { get; set; }
+    }
+}
