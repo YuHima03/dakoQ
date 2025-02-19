07911d1b4e96f5cd4544e8108eba63a004266983
YuHima03 <yuhima03@gmail.com>
2025-02-20 01:24:29 +0900

    ✨ IRoomDataSourcesRepository の実装

---
+++ src/Dakoq.Repository/Implements/RoomDataSourcesRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dakoq.Repository.Exceptions;
+using Dakoq.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dakoq.Repository
+{
+    public sealed partial class RepositoryContext : IRoomDataSourcesRepository
+    {
+        public async ValueTask<List<KeyValuePair<int, string>>> GetActiveRoomDataSourceMappingAsync(CancellationToken ct = default)
+        {
+            return await RoomDataSources.Select(r => KeyValuePair.Create(r.Id, r.Name)).ToListAsync(ct);
+        }
+
+        public async ValueTask<RoomDataSource> GetRoomDataSourceAsync(int id, CancellationToken ct = default)
+        {
+            return await RoomDataSources.Where(r => r.Id == id).SingleOrDefaultAsync(ct) ?? throw new SqlRowNotFoundException();
+        }
+
+        public async ValueTask<RoomDataSource> GetRoomDataSourceAsync(string name, CancellationToken ct = default)
+        {
+            return await RoomDataSources.Where(r => r.Name == name).SingleOrDefaultAsync(ct) ?? throw new SqlRowNotFoundException();
+        }
+    }
+}
