8447ab4bf9f05f2e36b012571b5a6199dc0e03f7
YuHima03 <yuhima03@gmail.com>
2025-02-20 01:28:06 +0900

    🎨 dotnet format

--- src/Dakoq.Repository/IRoomDataSourcesRepository.cs
+++ src/Dakoq.Repository/IRoomDataSourcesRepository.cs
@@ -4,6 +4,6 @@
     {
         public ValueTask<List<KeyValuePair<int, string>>> GetActiveRoomDataSourceMappingAsync(CancellationToken ct = default);
 
-        public ValueTask<Models.RoomDataSource> GetRoomDataSourceAsync(int id, CancellationToken ct = default); 
+        public ValueTask<Models.RoomDataSource> GetRoomDataSourceAsync(int id, CancellationToken ct = default);
     }
 }
