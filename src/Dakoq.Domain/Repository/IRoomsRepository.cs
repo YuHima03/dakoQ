88222a60d4df1f2e9ed0888287837d0e09bd8bdf
YuHima03 <yuhima03@gmail.com>
2025-05-02 18:19:32 +0900

    ✨ IRoomsRepository にメソッド追加

--- src/Dakoq.Domain/Repository/IRoomsRepository.cs
+++ src/Dakoq.Domain/Repository/IRoomsRepository.cs
@@ -4,7 +4,20 @@ namespace Dakoq.Domain.Repository
 {
     public interface IRoomsRepository
     {
+        public ValueTask AddRoomOpeningHoursAsync(Guid roomId, RoomHours[] openingHours, CancellationToken ct);
+
+        public ValueTask AddOrOverwriteOpeningHoursAsync(Guid roomId, RoomHours[] openingHours, CancellationToken ct);
+
+        public ValueTask DeleteRoomOpeningHoursAsync(Guid roomId, RoomHours[] excludedHours, CancellationToken ct);
+
         public ValueTask<Room[]> GetAvailableRoomsAsync(DateTimeOffset time, CancellationToken ct);
-        public ValueTask<Room[]> GetAvailableRoomsAsync(DateTimeOffset? after, DateTimeOffset? before, CancellationToken ct);
+        
+        public ValueTask<Room[]> GetAvailableRoomsAsync(RoomHours hours, CancellationToken ct);
+        
+        public ValueTask<RoomHoursWithSource[]> GetRoomOpeningHoursAsync(Guid roomId, DateTimeOffset after, DateTimeOffset before, CancellationToken ct);
+        
+        public ValueTask<Room> PostRoomAsync(PostRoomRequest request, CancellationToken ct);
+
+        public ValueTask<Room> UpdateRoomAsync(Guid roomId, PostRoomRequest request, CancellationToken ct);
     }
 }
