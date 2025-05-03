da1e12c5bce10b791c830407707bf69c5426bb65
YuHima03 <yuhima03@gmail.com>
2025-05-03 15:42:57 +0900

    ✨ v2 DB schema

--- db/init/0_schema.sql
+++ db/init/0_schema.sql
@@ -45,7 +45,62 @@ ALTER TABLE `old_room_participants` ADD INDEX `idx_user`               (`user_id
 ALTER TABLE `old_room_participants` ADD INDEX `idx_room_and_join_time` (`room_id`, `joined_at`);
 ALTER TABLE `old_room_participants` ADD INDEX `idx_user_and_join_time` (`user_id`, `joined_at`);
 
--- New DB schema
+-- v2 Schema
+--   v2.0.0 2025-05-01
+
+CREATE TABLE `rooms` (
+    `id`                CHAR(36)        NOT NULL                    PRIMARY KEY,
+    `name`              VARCHAR(255)    NOT NULL,
+    `description`       TEXT                        DEFAULT NULL,
+    `created_at`        DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`        DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at`        DATETIME                    DEFAULT NULL
+)   DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `rooms` ADD INDEX (`deleted_at`, `id`);
+ALTER TABLE `rooms` ADD INDEX (`deleted_at`);
+
+CREATE TABLE `room_admin_groups` (
+    `room_id`       CHAR(36)    NOT NULL,
+    `group_id`      CHAR(36)    NOT NULL    COMMENT 'traQ group uuid',
+    `created_at`    DATETIME    NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`)
+)   DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `room_admin_users` (
+    `room_id`       CHAR(36)    NOT NULL,
+    `user_id`       CHAR(36)    NOT NULL    COMMENT 'traQ user uuid',
+    `created_at`    DATETIME    NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`)
+)   DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `knoq_v1_rooms` (
+    `id`            CHAR(36)        NOT NULL    PRIMARY KEY     COMMENT 'knoQ room uuid',
+    `name`          VARCHAR(256)    NOT NULL,
+    `is_verified`   BOOLEAN         NOT NULL    DEFAULT 0,
+    `starts_at`     DATETIME        DEFAULT NULL,
+    `ends_at`       DATETIME        DEFAULT NULL,
+    `created_at`    DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP
+)   DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `knoq_v1_events` (
+    `id`            CHAR(36)        NOT NULL        PRIMARY KEY   COMMENT 'knoQ event uuid',
+    `name`          VARCHAR(256)    NOT NULL,
+    `room_id`       CHAR(36)        NOT NULL                      COMMENT 'knoQ room uuid',
+    `starts_at`     DATETIME        DEFAULT NULL,
+    `ends_at`       DATETIME        DEFAULT NULL,
+    `created_at`    DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (`room_id`) REFERENCES `knoq_v1_rooms`(`id`)
+)   DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `room_sources` (
+    `id`                INT UNSIGNED    NOT NULL        PRIMARY KEY     AUTO_INCREMENT,
+    `knoq_v1_event_id`  CHAR(36)        DEFAULT NULL,
+    `knoq_v1_room_id`   CHAR(36)        DEFAULT NULL,
+    FOREIGN KEY (`knoq_v1_event_id`) REFERENCES `knoq_v1_events`(`id`),
+    FOREIGN KEY (`knoq_v1_room_id`)  REFERENCES `knoq_v1_rooms`(`id`)
+)   DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `room_opening_horus` (
     `room_id`       CHAR(36)        NOT NULL,
@@ -55,34 +110,20 @@ CREATE TABLE `room_opening_horus` (
     FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`),
     FOREIGN KEY (`source_id`) REFERENCES `room_sources`(`id`)
 )   DEFAULT CHARSET=utf8mb4;
-ALTER TABLE `room_opening_horus` ADD INDEX `idx_room` (`room_id`);
+ALTER TABLE `room_opening_horus` ADD INDEX `idx_room`       (`room_id`);
 ALTER TABLE `room_opening_horus` ADD INDEX `idx_time_range` (`starts_at`, `ends_at`);
 
-CREATE TABLE `room_sources` (
-    `id`                        INT UNSIGNED    NOT NULL        PRIMARY KEY     AUTO_INCREMENT,
-    `knoq_v1_event_id`          CHAR(36)        DEFAULT NULL,
-    `knoq_v1_room_id`  CHAR(36)        DEFAULT NULL
-    FOREIGN KEY (`knoq_v1_event_id`) REFERENCES `knoq_v1_events`(`id`),
-    FOREIGN KEY (`knoq_v1_room_id`) REFERENCES `knoq_v1_verified_rooms`(`id`)
+CREATE TABLE `room_participants` (
+    `id`            INT UNSIGNED    NOT NULL    PRIMARY KEY     AUTO_INCREMENT,
+    `room_id`       CHAR(36)        NOT NULL,
+    `user_id`       CHAR(36)        NOT NULL                                COMMENT 'traQ user uuid',
+    `joined_at`     DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    `left_at`       DATETIME                    DEFAULT NULL,
+    `created_at`    DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`)
 )   DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `knoq_v1_events` (
-    `id`        CHAR(36)        NOT NULL        PRIMARY KEY   COMMENT 'knoQ event uuid',
-    `name`      VARCHAR(256)    NOT NULL,
-    `room_id`   CHAR(36)        NOT NULL                      COMMENT 'knoQ room uuid',
-    `starts_at`  DATETIME        DEFAULT NULL,
-    `ends_at`    DATETIME        DEFAULT NULL,
-    `created_at` DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`room_id`) REFERENCES `knoq_v1_rooms`(`id`)
-)   DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `knoq_v1_rooms` (
-    `id`            CHAR(36)        NOT NULL    PRIMARY KEY     COMMENT 'knoQ room uuid',
-    `name`          VARCHAR(256)    NOT NULL,
-    `is_verified`    BOOLEAN         NOT NULL    DEFAULT 0,
-    `starts_at`      DATETIME        DEFAULT NULL,
-    `ends_at`        DATETIME        DEFAULT NULL,
-    `created_at`     DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`     DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP   ON UPDATE CURRENT_TIMESTAMP
-)   DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `room_participants` ADD INDEX `idx_room`               (`room_id`);
+ALTER TABLE `room_participants` ADD INDEX `idx_user`               (`user_id`);
+ALTER TABLE `room_participants` ADD INDEX `idx_room_and_join_time` (`room_id`, `joined_at`);
+ALTER TABLE `room_participants` ADD INDEX `idx_user_and_join_time` (`user_id`, `joined_at`);
