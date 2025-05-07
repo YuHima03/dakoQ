7f27e1a5f1c8d9fce7068d9bd0955b4f759aafa3
YuHima03 <yuhima03@gmail.com>
2025-05-07 17:49:04 +0900

    🔥 DB スキーマ変更に伴い破壊

--- src/Dakoq.Infrastructure/Repository/Repository.cs
+++ src/Dakoq.Infrastructure/Repository/Repository.cs
@@ -5,19 +5,11 @@ namespace Dakoq.Infrastructure.Repository
 {
     public sealed partial class Repository(DbContextOptions<Repository> options) : DbContext(options), IRepository
     {
-        #region knoQ
-
-        DbSet<Models.KnoqV1Event> KnoqV1Events { get; set; }
-        DbSet<Models.KnoqV1Room> KnoqV1Rooms { get; set; }
-
-        #endregion
-
         DbSet<Models.Room> Rooms { get; set; }
         DbSet<Models.RoomAdminUser> RoomAdminUsers { get; set; }
         DbSet<Models.RoomAdminGroup> RoomAdminGroups { get; set; }
         DbSet<Models.RoomOpeningHours> RoomOpeningHours { get; set; }
         DbSet<Models.RoomParticipant> RoomParticipants { get; set; }
-        DbSet<Models.RoomSource> RoomSources { get; set; }
         DbSet<Models.RoomWebhook> RoomWebhooks { get; set; }
     }
 }
